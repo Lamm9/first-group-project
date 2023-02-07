b17f540446e5664238178fb0e45204a4ad46513e
Harrison <harrylamm@icloud.com>
2023-02-06 19:32:06 -0800

    added button event listeners

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,2 +1,11 @@
 $(function() {
+    var disneyBtn = $('#quiz1');
+    var marvelBtn = $('#quiz2');
+    var pokemonBtn = $('#quiz3');
+
+    $(disneyBtn).on('click',disneyQuiz());
+
+    $(marvelBtn).on('click',marvelQuiz());
+
+    $(pokemonBtn).on('click',pokemonQuiz());
 })
