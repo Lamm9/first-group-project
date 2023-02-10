db284f257549499f12689d014dcc7d942913860c
Harrison <harrylamm@icloud.com>
2023-02-09 20:37:17 -0800

    I changed it to star wars because the marvel api is dumb

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,11 +1,33 @@
 $(function() {
     var disneyBtn = $('#quiz1');
-    var marvelBtn = $('#quiz2');
+    var swBtn = $('#quiz2');
     var pokemonBtn = $('#quiz3');
 
-    $(disneyBtn).on('click',disneyQuiz());
+    var swURL = 'https://swapi.dev/api/';
 
-    $(marvelBtn).on('click',marvelQuiz());
+    swQuiz = function() {
+        window.location.href ="/sw-quiz.html";
 
-    $(pokemonBtn).on('click',pokemonQuiz());
+        fetch(swURL);
+
+        var swAnswer1 = $('#sw-answer-1');
+        var swAnswer2 = $('#sw-answer-2');
+        var swAsnwer3 = $('#sw-answer-3');
+        var swAnswer4 = $('#sw-answer-4');
+        var swImage = $('#sw-img');
+
+        swImage.href = 
+
+        $(swAnswer1)
+        $(swAnswer2)
+        $(swAsnwer3)
+        $(swAnswer4)
+
+    }
+
+    //$(disneyBtn).on('click',disneyQuiz());
+
+    $(swBtn).on('click',swQuiz());
+
+    //$(pokemonBtn).on('click',pokemonQuiz());
 })
