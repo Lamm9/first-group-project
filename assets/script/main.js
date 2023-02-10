b1fab09600d9793b8c8f8d77396ac711750415ec
Harrison <harrylamm@icloud.com>
2023-02-09 21:35:00 -0800

    started starwars quiz

--- assets/script/main.js
+++ assets/script/main.js
@@ -15,6 +15,33 @@ $(function() {
         var swAsnwer3 = $('#sw-answer-3');
         var swAnswer4 = $('#sw-answer-4');
         var swImage = $('#sw-img');
+        var swQuestion = $('#sw-question');
+        
+        var content = [
+        {question: 'What lightsaber color best suits you?',
+
+        answers: ['Blue', 'Green','Red','Purple']
+    
+        },
+    
+        {question: 'Which Jedi master would you train with?',
+
+        answers: ['2']
+    
+        },
+
+        {question: 'What species would your space companion be?',
+    
+        answers: ['3']
+
+        },
+
+        {question: 'What faction would you fight for?',
+    
+        answers: ['4']
+    
+        },
+    ]
 
         swImage.href = 
 
@@ -22,6 +49,15 @@ $(function() {
         $(swAnswer2)
         $(swAsnwer3)
         $(swAnswer4)
+
+        for (i=0;i<content.length;i++) {
+            $(swQuestion).append('<h2>'+content[i].question+'</h2>');
+
+            $(swAnswer1).append('<li>'+content[0].answers[0]+'</li>')
+            $(swAnswer2).textContent = content[0].answers[1]
+            $(swAsnwer3).textContent = content[0].answers[2]
+            $(swAnswer4).textContent = content[0].answers[3]
+        }
 
     }
 
